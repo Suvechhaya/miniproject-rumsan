0c69206c513805b65c4b33bfd822835c41a1a649
Suvechhaya <41965913+Suvechhaya@users.noreply.github.com>
2019-04-30 18:48:58 +0545

    second commit

--- routes/scripts.js
+++ routes/scripts.js
@@ -13,26 +13,3 @@ $(function()){
           }
       })
     })
-// $.ajax({  
-//        type: "POST",  
-//        url: "http://localhost/viewDetails",  
-//        dataType: "json",  
-//        success: function(resp){  
-
-//           var tbodyEl = $('tbody');
-//           tbodyEl.ejs('');
-
-//           response.users.forEach(function(users){
-//             tbodyEl.append('\
-//               <tr>\
-//                 <td class="id">' + product.id + '</td>\ 
-//                 <td><input type="text" class="name" value="'+ product.name +
-//                   '"></td>\
-// );
-//           })
-//          console.log("resp", resp);  
-//        },  
-//        error: function(e){  
-//          alert('error', e);  
-//        }  
-//      });
